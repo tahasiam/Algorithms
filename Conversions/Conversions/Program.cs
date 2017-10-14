a8dc7ba5ccb7fcb15ff27321efee6c5dbfc6f5ec
Taha <tahasiamwala@gmail.com>
2017-10-14 23:10:31 +0300

    Starting Commit

---
+++ Conversions/Conversions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CallForBinary();
+            Console.ReadLine();
+        }
+        
+        public static void CallForBinary()
+        {
+            Utilities util = new Utilities();
+            Console.Write("Please enter  a integer value : ");
+            int input =  Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Console.Write("The Binary equivalent is : ");
+            util.IntToBinary(input);
+        }
+    }
+    
+}
---
+++ Conversions/Conversions/Utilities.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversions
+{
+    class Utilities
+    {
+        public void IntToBinary(int input)
+        {
+            List<Int32> binary = new List<Int32>();
+
+            while (input > 0)
+            {
+                binary.Add(input % 2);
+                input = input / 2;
+            }
+            for (int i = binary.Count - 1; i >= 0; i--)
+            {
+                Console.Write(binary[i]);
+            }
+        }
+    }
+}
